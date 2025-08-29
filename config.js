8ef3ffb38a7ca7449795dbba5dd562764dff14de
i-ledger <newledger2024@gmail.com>
2025-08-29 16:01:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_ENDPOINT = "https://script.google.com/macros/s/AKfycbz553cdlU67op0mlJaGJds9sItEstVHv1haIVT19Nepo1e3b_5ZJD9MLZj26z3q3cuN/exec";
+const API_ENDPOINT = "https://script.google.com/macros/s/AKfycbzi_-e8fBG9gbsaQT5HhJVUr9552yZRE8-DTomdOrlWNUj7KUq43y3mvYyzm5vcaydo/exec";
