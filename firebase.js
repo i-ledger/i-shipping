e943025f0ae783b78d37dd0816ba128ab1a901de
i-ledger <newledger2024@gmail.com>
2026-04-01 13:58:38 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,27 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCtLh42xjTfCkak6G3vCj0zLGR3aht3iR4",
+  authDomain: "i-shipping.firebaseapp.com",
+  projectId: "i-shipping",
+  storageBucket: "i-shipping.firebasestorage.app",
+  messagingSenderId: "22272415602",
+  appId: "1:22272415602:web:56db1f7e871dba9ff72feb",
+  measurementId: "G-5EZSK8CT9G"
+};
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+Notification.requestPermission().then(permission=>{
+if(permission==="granted"){
+
+messaging.getToken().then(token=>{
+
+console.log("TOKEN:",token);
+
+localStorage.setItem("fcmToken",token);
+
+});
+
+}
+});
