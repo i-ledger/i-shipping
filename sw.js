b79732eeb7d326f71712ea6d6889dce9f8bbf816
i-ledger <newledger2024@gmail.com>
2026-04-01 13:41:01 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  return self.clients.claim();
+});
+
+self.addEventListener("notificationclick", function(event){
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow("dashboard.html")
+  );
+
+});
